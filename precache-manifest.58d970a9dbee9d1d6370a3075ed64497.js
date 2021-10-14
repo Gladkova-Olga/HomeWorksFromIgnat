e665a3da000364aa511451faef8b49db3e0dd4f3
Lisla Fox <lisla@Lislas-MBP.home>
2021-10-14 11:01:50 +0300

    Updates

---
+++ precache-manifest.58d970a9dbee9d1d6370a3075ed64497.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f00d37387d34f7ef60645cfa5d5781",
+    "url": "/HomeWorksFromIgnat/index.html"
+  },
+  {
+    "revision": "01189f05786d87a27d88",
+    "url": "/HomeWorksFromIgnat/static/css/main.c544655c.chunk.css"
+  },
+  {
+    "revision": "12d0046fd0ec7b2fc2d8",
+    "url": "/HomeWorksFromIgnat/static/js/2.95a57bec.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/HomeWorksFromIgnat/static/js/2.95a57bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01189f05786d87a27d88",
+    "url": "/HomeWorksFromIgnat/static/js/main.12803365.chunk.js"
+  },
+  {
+    "revision": "a890f6dbc24bc5542934",
+    "url": "/HomeWorksFromIgnat/static/js/runtime-main.a38eb9ba.js"
+  },
+  {
+    "revision": "e275bf6b681fd7108a7ddcd22b640097",
+    "url": "/HomeWorksFromIgnat/static/media/loader.e275bf6b.gif"
+  }
+]);
